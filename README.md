70ae833dedf93cfe293364d94683cc6947aaa4c7
Ole Andreas Jørnsen Herland <ole.gogoro@gmail.com>
2023-06-09 15:40:01 +0200

    Version one is done

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# python-compress-video
+# Python Compress Video
+
+## Gaol
+I have 5TB+ worth of videos stored on my NAS that are from livestreams I've done and YouTube videos.
+
+Compressing all of these videos using Media Encoder is going to take a while and be cluncy. Also, I want this to just happen automatically whenever I save a recording.
+
+Therefore I'm going to make the following functionality:
+- [x] Distinguish between compressed and not compressed files
+- [x] Set root folder, and automatically compress ALL videos in sub folders of this
+- [x] Have good naming, so I can visually see what's been compressed. // Dropped this. Program detects if it needs to be compressed, that's enough.
+
+## Usage
+
+### Warning
+This script runs through all the sub folders to the base folder you define and finds `.mp4` files and compresses them. After it's done running, it deletes the original and replaces that with the new file. 
+So, if the process failes. It should not delete the original. 
+But if the process is successful, you will loose your original file.
+
+### Running the command
+1. Install `ffmpeg`
+2. Instal dependencies `python3 pip install -r requirements.txt`
+2. Run compression with `python3 compress-video.py test/` // Change `test/` to your base path.
--- compress-video.py
+++ compress-video.py
@@ -1,3 +1,46 @@
+import os
 import ffmpeg
+import subprocess
+import sys
 
-ffmpeg.input('test/original.mp4').output('test/compressed.mp4', **{'c:v': 'h264_videotoolbox', 'c:a': 'aac', 'b:v': '6800k'}).run()
+# Get input from the user with the path to the directory with the videos
+if len(sys.argv) == 1:
+    print('Please provide a path to the directory with the videos')
+    sys.exit()
+
+print('Compressing videos in: ', sys.argv[1])
+basepath = sys.argv[1]
+
+# List all files in a directory using os.listdir
+files_in_basepath = os.listdir(basepath)
+
+# Filter out all files that are not video files
+# Filter out all files that are already compressed
+# Filter out all files that has a compressed version
+files_in_basepath = [file for file in files_in_basepath if file.endswith('.mp4') and not file.startswith('compressed-') and not 'compressed-' + file in files_in_basepath]
+
+# Fill this variable with files that we are going to compress 
+files_to_compress = []
+
+# Check if file is not compressed and is a video file
+for file_in_basepath in files_in_basepath:
+    cmd = ['ffprobe', '-v', 'error', '-select_streams', 'v:0', '-show_entries', 'stream=bit_rate', '-of', 'default=noprint_wrappers=1:nokey=1', basepath + file_in_basepath]
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = process.communicate()[0]
+    print(output)
+    if int(output) < 6850000:
+        print('File is already compressed', file_in_basepath)
+    elif file_in_basepath.endswith('.mp4'):
+        files_to_compress.append(file_in_basepath)
+
+# Compress all files in the list
+print('Files to compress: ', files_to_compress)
+for file_to_compress in files_to_compress:
+    print('Compressing: ', file_to_compress)
+    # Compression command
+    ffmpeg.input(basepath + file_to_compress).output(basepath + 'compressed-' + file_to_compress, **{'c:v': 'h264_videotoolbox', 'c:a': 'aac', 'b:v': '6800k'}).run()
+    # Delete the original file
+    os.remove(basepath + file_to_compress)
+    # Rename the compressed file to the original name
+    os.rename(basepath + 'compressed-' + file_to_compress, basepath + file_to_compress)
+    print('Done compressing: ', file_to_compress)
